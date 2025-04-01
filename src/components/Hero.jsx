73fccf4baf653f76fb09c872d7448a985d800b81
Prem Sangle <prem.s@weqtechnologies.com>
2025-04-01 10:21:08 +0530

    Fixed overlapping issue in ComputersCanvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,9 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      <div className="mt-20"> {/* Increased margin-top */}
+        <ComputersCanvas />
+      </div>
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
