1d833caacde8a68194f8408d6c8e241228d1a81d
Prem Sangle <prem.s@weqtechnologies.com>
2025-04-01 16:30:20 +0530

    Mobile fixes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,125 +1,15 @@
-// import React, { Suspense, useEffect, useState } from "react";
-// import { Canvas } from "@react-three/fiber";
-// import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
-// import CanvasLoader from "../Loader";
-
-// const Computers = ({ isMobile }) => {
-//   const computer = useGLTF("./desktop_pc/scene.gltf");
-
-//   return (
-//     <mesh>
-//       <hemisphereLight intensity={0.15} groundColor='black' />
-//       <spotLight
-//         position={[-20, 50, 10]}
-//         angle={0.12}
-//         penumbra={1}
-//         intensity={1}
-//         castShadow
-//         shadow-mapSize={1024}
-//       />
-//       <pointLight intensity={1} />
-//       <primitive
-//         object={computer.scene}
-//         scale={isMobile ? 0.7 : 0.75}
-//         // scale={isMobile ? 0.5 : 0.65}
-//         position={isMobile ? [0, -3, -2.2] : [0, -4, -1.5]}
-//         rotation={[-0.01, -0.2, -0.1]}
-//       />
-//     </mesh>
-//   );
-// };
-
-// const ComputersCanvas = () => {
-//   const [isMobile, setIsMobile] = useState(false);
-
-//   useEffect(() => {
-//     // Add a listener for changes to the screen size
-//     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-//     // Set the initial value of the `isMobile` state variable
-//     setIsMobile(mediaQuery.matches);
-
-//     // Define a callback function to handle changes to the media query
-//     const handleMediaQueryChange = (event) => {
-//       setIsMobile(event.matches);
-//     };
-
-//     // Add the callback function as a listener for changes to the media query
-//     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-//     // Remove the listener when the component is unmounted
-//     return () => {
-//       mediaQuery.removeEventListener("change", handleMediaQueryChange);
-//     };
-//   }, []);
-
-//   return (
-//     <Canvas
-//       frameloop='demand'
-//       shadows
-//       dpr={[1, 2]}
-//       camera={{ position: [20, 3, 5], fov: 25 }}
-//       // camera={{ position: [10, 3, 5], fov: 20 }} // Adjusted for mobile
-//       gl={{ preserveDrawingBuffer: true }}
-//       // gl={{ version: 1 }} // Force WebGL1
-//     >
-//       <Suspense fallback={<CanvasLoader />}>
-//         <OrbitControls
-//           enableZoom={false}
-//           maxPolarAngle={Math.PI / 2}
-//           minPolarAngle={Math.PI / 2}
-//         />
-//         <Computers isMobile={isMobile} />
-//       </Suspense>
-
-//       <Preload all />
-//     </Canvas>
-//   );
-// };
-
-// export default ComputersCanvas;
-
-
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-// Function to sanitize geometry data (replace NaN values)
-const sanitizeGeometry = (geometry) => {
-  const positionAttribute = geometry.attributes.position;
-  for (let i = 0; i < positionAttribute.count; i++) {
-    // Check for NaN values in position attributes and replace with 0
-    if (isNaN(positionAttribute.getX(i))) {
-      positionAttribute.setX(i, 0); // Default to 0 if NaN
-    }
-    if (isNaN(positionAttribute.getY(i))) {
-      positionAttribute.setY(i, 0);
-    }
-    if (isNaN(positionAttribute.getZ(i))) {
-      positionAttribute.setZ(i, 0);
-    }
-  }
-  // Mark the attribute as needing update after modifying
-  geometry.attributes.position.needsUpdate = true;
-};
-
 const Computers = ({ isMobile }) => {
-  const { scene } = useGLTF("./desktop_pc/scene.gltf");
-
-  // Sanitize geometry after loading the model
-  useEffect(() => {
-    if (scene && scene.children.length > 0) {
-      const geometry = scene.children[0].geometry;
-      sanitizeGeometry(geometry);
-    }
-  }, [scene]);
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -130,8 +20,9 @@ const Computers = ({ isMobile }) => {
       />
       <pointLight intensity={1} />
       <primitive
-        object={scene}
+        object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
+        // scale={isMobile ? 0.5 : 0.65}
         position={isMobile ? [0, -3, -2.2] : [0, -4, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -165,11 +56,13 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }} // Keep the drawing buffer for mobile compatibility
+      // camera={{ position: [10, 3, 5], fov: 20 }} // Adjusted for mobile
+      gl={{ preserveDrawingBuffer: true }}
+      // gl={{ version: 1 }} // Force WebGL1
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
